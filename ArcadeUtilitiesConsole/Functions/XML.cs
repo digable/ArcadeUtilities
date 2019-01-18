4c2e416e91291ee73ff8b9039b08e2ca49751cc2
quincy <quincy.fuller@gmail.com>
2019-01-18 11:49:09 -0600

    updates to xml parsing

---
+++ ArcadeUtilitiesConsole/Functions/XML.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ArcadeUtilitiesConsole.Functions
+{
+    class XML
+    {
+        public static bool ValidateFile(string file)
+        {
+            bool b = true;
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(file);
+                doc = null;
+            }
+            catch(Exception ex)
+            {
+                b = false;
+            }
+
+            file = null;
+
+            return b;
+        }
+        public class LEDBlinkyControls
+        {
+            //TODO: need to figure a way to validate xml files when this class is called
+            public class ExtractData
+            {
+                public static List<string> RomNames(string file)
+                {
+                    List<string> l = new List<string>();
+
+                    //INFO: path --> root--><dat><emulator emuname="MAME" emuDesc="MAME Defaults and Control Overrides">
+                    //then get the nodes below called <controlGroup groupName="DEFAULT"
+                    //groupname is the rom name
+                    return l;
+                }
+            }
+        }
+    }
+}
--- ArcadeUtilitiesConsole/Program.cs
+++ ArcadeUtilitiesConsole/Program.cs
@@ -49,6 +49,25 @@ namespace ArcadeUtilitiesConsole
             }
 
             //INFO: LEDBlinkyControls.xml --> this is the first place it'll check for colors
+            string LEDBlinkyControlsFile = ConfigurationManager.AppSettings["LEDBlinkyControlsFile"].Trim();
+            string filePathLEDBlinkyControlsXML = LEDBlinkyDirectory + @"\" + LEDBlinkyControlsFile;
+            Dictionary<string, List<Dictionary<string, object>>> LEDBlinkyControlsXML = new Dictionary<string, List<Dictionary<string, object>>>();
+            List<string> LEDBlinkyControlsRoms = new List<string>();
+            if (File.Exists(filePathLEDBlinkyControlsXML))
+            {
+                //INFO: validate xml file
+                bool b = Functions.XML.ValidateFile(filePathLEDBlinkyControlsXML);
+
+                if (b)
+                {
+                    //TODO: get roms
+                    LEDBlinkyControlsRoms = Functions.XML.LEDBlinkyControls.ExtractData.RomNames(filePathLEDBlinkyControlsXML);
+                    //LEDBlinkyControlsXML = Functions.XML.LEDBlinkyControls.ExtractData(filePathLEDBlinkyControlsXML);
+                }
+            }
+            //should probably use xpath for specific information on this, get the roms first, then start pulling controls and colors
+            //<dat><emulator>
+            //<dat><emulator><controlgroup>
         }
     }
 }
